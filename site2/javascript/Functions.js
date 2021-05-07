63cf0a4ee83e915fdeb1667fb04fea3070648104
Oliver Yunior Mendez <olivermendezpersonal9@gmail.com>
2021-05-06 23:08:03 -0400

    Functions added

---
+++ site2/javascript/Functions.js
@@ -0,0 +1,36 @@
+//How to write functions in js
+
+var areas = [];
+
+function area(len, wid){
+    return len * wid;
+}
+
+var area1 = area(10,15);
+var area2 = area(10,25);
+var area3 = area(10,45);
+
+areas.push(area1);
+areas.push(area2);
+areas.push(area3);
+
+console.log(areas);
+
+
+
+var bankBalance = 1000;
+
+function makeTransactions(priceOfSale){
+
+    if(priceOfSale <= bankBalance){
+        bankBalance -=priceOfSale;
+        console.log("Purchase successful!");
+        console.log("Before, your actual balance is: "+ bankBalance);
+    }else{
+        console.log("Insufficient Funds :(");
+    }
+}
+
+console.log("Your actual bank balance is: "+ bankBalance);
+makeTransactions(79.00);
+makeTransactions(1022);
