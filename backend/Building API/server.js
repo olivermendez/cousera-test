4e5d901068d2cca4ed6c04d4376ac467a8a82f87
Oliver Yunior Mendez <olivermendezpersonal9@gmail.com>
2021-05-08 19:18:19 -0400

    I created my firts server in node

--- backend/Building API/server.js
+++ backend/Building API/server.js
@@ -6,6 +6,14 @@ app.get('/',function(request ,response){
     response.send('My Firts API');
 });
 
+app.get('/oliver',function(request, response){
+    response.send('Estas en la seccion de Oliver!');
+});
+
+app.get('/ol',function(request, response){
+    response.send('Estas en la seccion de Ol');
+});
+
 app.listen(3000, function(){
     console.log("Firts api running on port 3000!");
 });
